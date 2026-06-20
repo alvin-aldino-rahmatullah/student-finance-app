124b6aca72cce9e0495d33cb367b2641fccb2684
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-06-20 11:17:57 +0700

    Initial commit

---
+++ src/test/java/com/financetracker/student_finance_app/StudentFinanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financetracker.student_finance_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentFinanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
